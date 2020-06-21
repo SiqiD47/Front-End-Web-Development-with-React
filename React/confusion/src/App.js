f5bfdfef22720c12e25ecb9286903c900c19fd46
siqidai97@gmail.com <siqidai97@gmail.com>
2020-06-21 16:21:20 -0400

    react router

--- React/confusion/src/App.js
+++ React/confusion/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import './App.css';
+import { BrowserRouter } from 'react-router-dom';
 
 // use 'className' instead of 'class'
 
@@ -8,9 +9,11 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Main />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Main />
+        </div>
+      </BrowserRouter>
     );
   }
 }
